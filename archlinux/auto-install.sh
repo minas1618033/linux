ec65d4962ddd46f9971b47a47bd35a92a0bf8d30
Zelko Rocha <47934926+minas1618033@users.noreply.github.com>
2020-03-22 18:20:39 +0800

    2020-03-22 Ver 2.1
    Modify : replace trizen by yay
    Modify : comment some apps

--- archlinux/auto-install.sh
+++ archlinux/auto-install.sh
@@ -1,9 +1,12 @@
-# !/bin/bash 
+# !/bin/bash
+
 sudo sh -c "echo "" >> /etc/pacman.conf"
 sudo sh -c "echo "[archlinuxcn]" >> /etc/pacman.conf"
 sudo sh -c "echo "Server = https://mirrors.ustc.edu.cn/archlinuxcn/$arch" >> /etc/pacman.conf"
+
 sudo pacman -Syyu
 sudo pacman -S --noconfirm archlinuxcn-keyring
+sudo pacman -S --noconfirm unzip-iconv
 sudo pacman -S --noconfirm xorg-server
 sudo pacman -S --noconfirm nvidia
 sudo pacman -S --noconfirm bluedevil
@@ -42,24 +45,20 @@ sudo pacman -S --noconfirm user-manager
 sudo pacman -S --noconfirm xdg-desktop-portal-kde
 sudo pacman -S --noconfirm ark
 sudo pacman -S --noconfirm baidupcs-go
-sudo pacman -S --noconfirm base-devel
-sudo pacman -S --noconfirm bash-completion
+sudo pacman -S --noconfirm binutils
+sudo pacman -S --noconfirm bison
 sudo pacman -S --noconfirm bleachbit
 sudo pacman -S --noconfirm clipgrab
+sudo pacman -S --noconfirm code
 sudo pacman -S --noconfirm cronie
 sudo pacman -S --noconfirm dolphin
 sudo pacman -S --noconfirm dolphin-plugins
-sudo pacman -S --noconfirm exfat-utils
+sudo pacman -S --noconfirm fakeroot
 sudo pacman -S --noconfirm ffmpegthumbs
-sudo pacman -S --noconfirm ffnvcodec-headers
-sudo pacman -S --noconfirm kget
 sudo pacman -S --noconfirm gimp
 sudo pacman -S --noconfirm git
-sudo pacman -S --noconfirm grub-theme-vimix
 sudo pacman -S --noconfirm htop
-sudo pacman -S --noconfirm hunspell
 sudo pacman -S --noconfirm ibus
-sudo pacman -S --noconfirm kaccounts-integration
 sudo pacman -S --noconfirm kate
 sudo pacman -S --noconfirm kcalc
 sudo pacman -S --noconfirm kdegraphics-thumbnailers
@@ -74,70 +73,74 @@ sudo pacman -S --noconfirm kompare
 sudo pacman -S --noconfirm konsole
 sudo pacman -S --noconfirm krename
 sudo pacman -S --noconfirm ksystemlog
-sudo pacman -S --noconfirm libmpcdec
 sudo pacman -S --noconfirm libreoffice-still
 sudo pacman -S --noconfirm libreoffice-still-zh-tw
-sudo pacman -S --noconfirm libva-mesa-driver
-sudo pacman -S --noconfirm libva-vdpau-driver
-sudo pacman -S --noconfirm man-db
-sudo pacman -S --noconfirm man-pages
-sudo pacman -S --noconfirm mpg123
 sudo pacman -S --noconfirm mpv
-sudo pacman -S --noconfirm mtpfs
 sudo pacman -S --noconfirm noto-fonts-cjk
 sudo pacman -S --noconfirm okular
-sudo pacman -S --noconfirm openssh
 sudo pacman -S --noconfirm opera
 sudo pacman -S --noconfirm opera-ffmpeg-codecs
+sudo pacman -S --noconfirm p7zip
 sudo pacman -S --noconfirm partitionmanager
+sudo pacman -S --noconfirm patch
 sudo pacman -S --noconfirm profile-sync-daemon
-sudo pacman -S --noconfirm pulseaudio-alsa
-sudo pacman -S --noconfirm pulseaudio-bluetooth
 sudo pacman -S --noconfirm qmmp
 sudo pacman -S --noconfirm qt5-imageformats
 sudo pacman -S --noconfirm rclone
 sudo pacman -S --noconfirm rsync
-sudo pacman -S --noconfirm rtmpdump
 sudo pacman -S --noconfirm skanlite
+sudo pacman -S --noconfirm spectacle
 sudo pacman -S --noconfirm unrar
-sudo pacman -S --noconfirm wildmidi
-sudo pacman -S --noconfirm wine
 sudo pacman -S --noconfirm xdg-user-dirs
 sudo pacman -S --noconfirm yakuake
 sudo pacman -S --noconfirm youtube-dl
 
 sudo pacman -S --noconfirm anydesk
 sudo pacman -S --noconfirm crow-translate
+sudo pacman -S --noconfirm megatools
 sudo pacman -S --noconfirm qbittorrent-enhanced-git
 sudo pacman -S --noconfirm qview
+sudo pacman -S --noconfirm yay
 sudo pacman -S virtualbox
 
 # sudo pacman -S --noconfirm cups
+# sudo pacman -S --noconfirm exfat-utils
+# sudo pacman -S --noconfirm faad2 (qmmp)
+# sudo pacman -S --noconfirm grub-theme-vimix
 # sudo pacman -S --noconfirm k3b
-# sudo pacman -S --noconfirm kwayland-integration
+# sudo pacman -S --noconfirm kaccounts-integration
 # sudo pacman -S --noconfirm kaccounts-providers
-# sudo pacman -S --noconfirm kipi-plugins
-# sudo pacman -S --noconfirm spectacle
+# sudo pacman -S --noconfirm kwayland-integration
+# sudo pacman -S --noconfirm libmpcdec (qmmp)
+# sudo pacman -S --noconfirm libva-vdpau-driver (vlc)
+# sudo pacman -S --noconfirm man-db
+# sudo pacman -S --noconfirm man-pages
+# sudo pacman -S --noconfirm mpg123 (qmmp)
+# sudo pacman -S --noconfirm mtpfs
+# sudo pacman -S --noconfirm opusfile (qmmp)
+# sudo pacman -S --noconfirm pulseaudio-alsa
+# sudo pacman -S --noconfirm pulseaudio-bluetooth
 # sudo pacman -S --noconfirm udiskie
+# sudo pacman -S --noconfirm unzip-natspec
 # sudo pacman -S --noconfirm usb_modeswitch
+# sudo pacman -S --noconfirm wildmidi (qmmp)
+# sudo pacman -S --noconfirm wine
 
-git clone https://aur.archlinux.org/trizen.git
-cd trizen
-makepkg -si
+# git clone https://aur.archlinux.org/trizen.git
+# cd trizen
+# makepkg -si
 
-trizen -S ibus-libzhuyin
-trizen -S kde-servicemenus-rootactions
-trizen -S ksnip
-trizen -S megacmd-bin
-trizen -S ms-office-online
-trizen -S safeeyes
+yay -S ibus-libzhuyin
+yay -S kde-servicemenus-rootactions
+yay -S ms-office-online
+yay -S safeeyes
+yay -S rclone-browser
+yay -S wine-x64
+
+# trizen -S imagewriter
+# trizen -S ksnip
+# trizen -S megacmd-bin
 # trizen -S stacer
-trizen -S rclone-browser
-
-sudo pacman -Rsn --noconfirm gnome-common intltool wget
-
-sudo pacman -Rsn --noconfirm ccache cmake jsoncpp libuv rhash-1.3.9-1 extra-cmake-modules
-sudo pacman -Rsn --noconfirm python-cachecontrol python-chardet python-colorama python-contextlib python-distlib python-distro python-html5lib python-idna python-importlib-metadata python-msgpack python-pep517 python-progress python-pytoml python-requests python-retrying python-urllib3 python-webencodings python-zipp python-pip
 
 # sudo pacman -Rsn --noconfirm xdg-user-dirs
 
