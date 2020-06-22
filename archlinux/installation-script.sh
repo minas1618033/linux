25bcdb3f2a5090d3e6ceca9c3a3991c26ec7d5a3
Zelko Rocha <47934926+minas1618033@users.noreply.github.com>
2020-06-22 21:21:50 +0800

    2020-06-22 v0.3.0
    Alpha version

--- archlinux/installation-script.sh
+++ archlinux/installation-script.sh
@@ -6,14 +6,66 @@ echo "
 
 "
 # 0.Download installation script
-#     (wireless network) ip link set wlan0 up
-#     (wireless network) wifi-menu
-#     curl -L https://github.com/minas1618033/linux/archive/master.zip --output scripts.zip
-#     bsdtar -x -f scripts.zip
-#     chmod +x /root/linux-master/archlinux/*.sh
-#     sh /root/linux-master/archlinux/installation-script.sh
+# 	(wireless network) ip link set wlan0 down
+# 	(wireless network) wifi-menu
+# 	systemctl disable systemd-resolved
+# 	rm /etc/resolv.conf # removes symlink to /run
+# 	echo "nameserver 168.95.192.1" >> /etc/resolv.conf
+# 	curl -L https://github.com/minas1618033/linux/archive/master.zip --output scripts.zip
+# 	bsdtar -x -f scripts.zip
+# 	chmod +x /root/linux-master/archlinux/*.sh
+# 	sh /root/linux-master/archlinux/installation-script.sh
 
 	printf "   1.Normal Insatllation\n   2.Custom Installation\n   \n"
+
+	function selection {
+		ESC=$( printf "\033")
+		cursor_blink_on()  { printf "$ESC[?25h"; }
+		cursor_blink_off() { printf "$ESC[?25l"; }
+		cursor_to()        { printf "$ESC[$1;${2:-1}H"; }
+		print_option()     { printf "   $1 "; }
+		print_selected()   { printf "  $ESC[7m $1 $ESC[27m"; }
+		get_cursor_row()   { IFS=';' read -sdR -p $'\E[6n' ROW COL; echo ${ROW#*[}; }
+		key_input()        { read -s -n3 key 2>/dev/null >&2
+							 if [[ $key = $ESC[A ]]; then echo up;    fi
+							 if [[ $key = $ESC[B ]]; then echo down;  fi
+							 if [[ $key = ""     ]]; then echo enter; fi; }
+
+		# initially print empty new lines (scroll down if at bottom of screen)
+		for opt; do printf "\n"; done
+
+		# determine current screen position for overwriting the options
+		local lastrow=`get_cursor_row`
+		local startrow=$(($lastrow - $#))
+
+		# ensure cursor and input echoing back on upon a ctrl+c during read -s
+		trap "cursor_blink_on; stty echo; printf '\n'; exit" 2
+		cursor_blink_off
+
+		local selected=0
+		while true; do
+			# print options by overwriting the last lines
+			local idx=0
+			for opt; do
+				cursor_to $(($startrow + $idx))
+				if [ $idx -eq $selected ]; then
+					print_selected "$opt"
+				else
+					print_option "$opt"
+				fi
+				((idx++))
+			done
+
+			# user key control
+			case `key_input` in
+				enter) break;;
+				up)    ((selected--));
+					   if [ $selected -lt 0 ]; then selected=$(($# - 1)); fi;;
+				down)  ((selected++));
+					   if [ $selected -ge $# ]; then selected=0; fi;;
+			esac
+		done
+	}
 
 # 1.Connect to the Internet
 	function step1 {
